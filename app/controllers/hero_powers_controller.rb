f1385be264a5b8de005a7c0e7b09f2003c1bf8f8
Asher <Asher.areba@gmail.com>
2023-04-02 17:08:06 +0300

    added some controllers

---
+++ app/controllers/hero_powers_controller.rb
@@ -0,0 +1,16 @@
+class HeroPowersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def create
+        hero_power = HeroPower.create!(hero_power_params)
+        render json: hero_power.hero, serializer: HeroPowerSerializer
+        # hero_power = HeroPower.create!(hero_power_params)
+        # hero = Hero.find(hero_power.hero_id)
+        # render json: hero, serializer: EachHeroPowerSerializer, status: :created
+    end
+
+    private
+    def hero_power_params
+        params.permit(:strength, :power_id, :hero_id)
+    end
+end
